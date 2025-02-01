d8d0e6d8f11ac0894b629aa8cd782a0cdfc9c259
Dias <diasdzamalov64@gmail.com>
2025-02-01 16:46:00 +0500

    paasaaasыaыо

--- lab3/class/classes6.py
+++ lab3/class/classes6.py
@@ -1,9 +1,14 @@
-def num():
-    x=int(input())
-    y=x**0.5
-    for i in range(2,int(y)+1):
-        if x%i==0:
-            print("не простое число")
-            return     
-    print("простое число")
-num()
+class Filter:
+    def prime(self, n):
+        if n <= 1:
+            return False
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
+    def fil(self, numbers):
+        return list(filter(lambda x: self.prime(x), numbers))
+prime_filter = Filter()
+num = [10, 13, 7, 4, 11, 16, 23, 8]
+prime_numbers = prime_filter.fil(num)
+print(prime_numbers)
