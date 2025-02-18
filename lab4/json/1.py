b2990b140d3b48eac04b34ff827715060d9c30ea
Dias <diasdzamalov64@gmail.com>
2025-02-18 22:13:22 +0500

    sssd

---
+++ lab4/json/1.py
@@ -0,0 +1,16 @@
+import json
+with open("lab4/json/sample-data.json", "r") as file:
+    data=json.load(file)
+print("Interface Status")
+print("=" *80)
+print(f"{'DN':<50} {'Description':<20} {'Speed':<7} {'MTU':<6}")
+print("-"*80)
+for item in data["imdata"]:
+    attributes=item["l1PhysIf"]["attributes"]
+    dn =attributes["dn"]
+    descr =attributes["descr"]  
+    if not descr:  
+        descr =""
+    speed =attributes["speed"]
+    mtu =attributes["mtu"]
+    print(f"{dn:<50} {descr:<20} {speed:<7} {mtu:<6}")
