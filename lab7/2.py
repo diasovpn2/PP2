c1c69ae29ca14e3520212015c59289e570a46e6c
Dias <diasdzamalov64@gmail.com>
2025-03-19 11:39:46 +0500

    mkssd

--- lab7/2.py
+++ lab7/2.py
@@ -1,83 +1,45 @@
-<<<<<<< HEAD
+import os
 import pygame
-from datetime import datetime
-import math
+
 pygame.init()
+pygame.mixer.init()
 
-weight, height = 600, 400
-H_weight, H_height = weight // 2, height // 2
-rad = H_height - 50
+playlist = [f for f in os.listdir() if f.endswith(".mp3")]
+current_track = 0
 
-screen = pygame.display.set_mode((weight, height))
-pygame.display.set_caption('di')
-clock = pygame.time.Clock()
+def play_music(track):
+    pygame.mixer.music.load(playlist[track])
+    pygame.mixer.music.play()
+    print(f"Playing: {playlist[track]}")
 
-bg = pygame.image.load("bg.png")
-bg = pygame.transform.scale(bg, (600, 400))
 
-hour_hand = pygame.image.load("min.png")
-minute_hand = pygame.image.load("sec.png")
+if playlist:
+    play_music(current_track)
+else:
+    print("No music files found in the folder.")
 
-def draw_rotated_image(image, angle, position, offset_x=0, offset_y=0):
-    rotated_image = pygame.transform.rotate(image, -angle)
-    rect = rotated_image.get_rect(center=(position[0] + offset_x, position[1] + offset_y))
-    screen.blit(rotated_image, rect.topleft)
-
-while True:
+running = True
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
+            running = False
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:  
+                if pygame.mixer.music.get_busy():
+                    pygame.mixer.music.pause()
+                    print("Paused")
+                else:
+                    pygame.mixer.music.unpause()
+                    print("Resumed")
+            elif event.key == pygame.K_s:  
+                pygame.mixer.music.stop()
+                print("Stopped")
+            elif event.key == pygame.K_n:  
+                current_track = (current_track + 1) % len(playlist)
+                play_music(current_track)
+            elif event.key == pygame.K_p:  
+                current_track = (current_track - 1) % len(playlist)
+                play_music(current_track)
 
-    t = datetime.now()
-    hour, minute, second = t.hour % 12, t.minute, t.second
-
-    screen.blit(bg, (0, 0))
-
-    draw_rotated_image(hour_hand, minute * 6, (H_weight, H_height), offset_x=10)  # Смещение минутной стрелки вправо
-    draw_rotated_image(minute_hand, second * 6, (H_weight, H_height))
-
-    pygame.display.flip()
-    clock.tick(20)
-=======
-import pygame
-from datetime import datetime
-import math
-pygame.init()
-
-weight, height = 600, 400
-H_weight, H_height = weight // 2, height // 2
-rad = H_height - 50
-
-screen = pygame.display.set_mode((weight, height))
-pygame.display.set_caption('di')
-clock = pygame.time.Clock()
-
-bg = pygame.image.load("bg.png")
-bg = pygame.transform.scale(bg, (600, 400))
-
-hour_hand = pygame.image.load("min.png")
-minute_hand = pygame.image.load("sec.png")
-
-def draw_rotated_image(image, angle, position, offset_x=0, offset_y=0):
-    rotated_image = pygame.transform.rotate(image, -angle)
-    rect = rotated_image.get_rect(center=(position[0] + offset_x, position[1] + offset_y))
-    screen.blit(rotated_image, rect.topleft)
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-
-    t = datetime.now()
-    hour, minute, second = t.hour % 12, t.minute, t.second
-
-    screen.blit(bg, (0, 0))
-
-    draw_rotated_image(hour_hand, minute * 6, (H_weight, H_height), offset_x=10)  # Смещение минутной стрелки вправо
-    draw_rotated_image(minute_hand, second * 6, (H_weight, H_height))
-
-    pygame.display.flip()
-    clock.tick(20)
->>>>>>> e63b64598f430e27656b86c7e8b1691cb67dcf30
+pygame.quit()
--- lab7/3.py
+++ lab7/3.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pygame
 import sys
 from pygame.locals import *
@@ -74,51 +73,3 @@ while True:
 
     pygame.display.update()
     FramePerSec.tick(FPS)
-=======
-import pygame
-import os
-
-pygame.init()
-pygame.mixer.init()
-
-playlist = [f for f in os.listdir() if f.endswith(".mp3")]
-current_track = 0
-
-
-def play_music(track):
-    pygame.mixer.music.load(playlist[track])
-    pygame.mixer.music.play()
-    print(f"Playing: {playlist[track]}")
-
-
-if playlist:
-    play_music(current_track)
-else:
-    print("No music files found in the folder.")
-
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                if pygame.mixer.music.get_busy():
-                    pygame.mixer.music.pause()
-                    print("Paused")
-                else:
-                    pygame.mixer.music.unpause()
-                    print("Resumed")
-            elif event.key == pygame.K_s:
-                pygame.mixer.music.stop()
-                print("Stopped")
-            elif event.key == pygame.K_n:
-                current_track = (current_track + 1) % len(playlist)
-                play_music(current_track)
-            elif event.key == pygame.K_p:
-                current_track = (current_track - 1) % len(playlist)
-                play_music(current_track)
-
-pygame.quit()
->>>>>>> e63b64598f430e27656b86c7e8b1691cb67dcf30
